536b25ab539adb85e59573986537ec01f23a21a7
lliviamartins <lliviasantospaz2019@gmail.com>
2026-03-26 10:48:50 -0300

    aula

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
 n1=int(input('Digite um número:'))
 n2=int(input('Digite outro número:'))
-print('A soma é:', n1+n2)
-print('Subtração é:', n1-n2)
-print('Multiplicação:', n1*n2)
+print(f'A soma de {n1} e {n2} é =',n1+n2)
+print(f'A subtração de {n1} e {n2} é =', n1-n2)
+print('A multiplicação de',n1,'e',n2,'é =', n1*n2)
 
--- desconto.py
+++ desconto.py
@@ -1,4 +1,5 @@
 antigo=float(input('Digite o preço:'))
 desconto=antigo/100*15
 novo=antigo-desconto
-print('Desconto é:',novo)
+print('O valor do desconto é:',desconto)
+print('O novo preço: R$',novo)
